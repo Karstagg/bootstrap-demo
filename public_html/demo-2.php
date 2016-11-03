011cf0b648d74ba9840473ff136b7e4705efb506
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-03 13:43:24 -0600

    added main content area

--- public_html/demo-2.php
+++ public_html/demo-2.php
@@ -4,40 +4,67 @@
 		<?php require_once("lib/head-utils.php");?>
 	</head>
 	<body>
-		<div class="container">
-			<!-- welcome section-->
-			<div class="row">
-				<div class="col-xs-12">
-					<h1>ようこそ！</h1>
+		<!-- begin header-->
+		<header>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Bootstrap-demo2</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link1</a></li>
+							<li><a href="#">link2</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</header>
+		<!-- begin main-->
+		<main>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="jumbotron">
+							<h1>ようこそ、マーザーファーカー</h1>
+							<p>お前はもう死んでいる</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="box" class="text-center">
+							テキストアリア一番
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="box" class="text-center">
+							テキストアリア二番
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="box" class="text-center">
+							テキストアリア三番
+						</div>
+					</div>
+				</div>
+				<div class="row">
+
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<p>俺の練習ページです。</p>
-				</div>
-				<div class="col-md-6">
-					<p>まだ下手でしょう。</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-3">
-					<p>ここはテキストエリア一番</p>
-				</div>
-				<div class="col-md-3">
-					<p>ここはテキストエリア二番</p>
-				</div>
-				<div class="col-md-3">
-					<p>ここはテキストエリア三番</p>
-				</div>
-				<div class="col-md-3">
-					<p>ここはテキストエリア四番</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<p><a href="demo-2.php">次へ</a></p>
-				</div>
-			</div>
-		</div>
+		</main>
+		<!-- begin footer-->
+		<footer>
+
+		</footer>
 	</body>
 </html>
